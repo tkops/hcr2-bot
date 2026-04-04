a124f290aef0174e4d421a543a137aebac62612f
Tobias Koch <pipuhop@gmail.com>
2026-04-04 10:03:13 +0200

    V0.7.0

--- version.py
+++ version.py
@@ -1,6 +1,7 @@
-VERSION = "0.6.2"
+VERSION = "0.7.0"
 
 HISTORY = [
+    ("0.7.0", "2026-04-04", "Stats showing all members with more than 3 matches for seasons stats"),
     ("0.6.2", "2026-03-29", "Simplify match add"),
     ("0.6.1", "2026-02-03", "sheet(donations): display amounts in k on export and import as thousands"),
     ("0.6.0", "2025-12-09", "Add player stats"),
